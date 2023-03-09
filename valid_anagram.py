b271be8d5d7c8d3e95e5cddfb800bd052849df16
Ali Sabanci <alisabanci7@gmail.com>
2023-03-09 10:53:57 +0300

    correction while adding dictionary

--- valid_anagram.py
+++ valid_anagram.py
@@ -6,14 +6,14 @@ def isAnagram(s, t):
     countS, countT = {}, {}
 
     for i in range(len(s)):
-        countS[i] = 1 + countS.get(s[i], 0)
-        countT[i] = 1 + countT.get(t[i], 0)
-
-    if countS.keys() != countT.keys():
-        return False
+        countS[s[i]] = 1 + countS.get(s[i], 0)
+        countT[t[i]] = 1 + countT.get(t[i], 0)
     
-    # for c in countS:
-    #     if countS[c] != countT.get(c, 0):
-    #         return False
+    for c in countS:
+        if countS[c] != countT.get(c, 0):
+            return False
+
+    # if countS.keys() != countT.keys():
+    #     return False
 
     return True
