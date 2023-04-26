ab519b4860487d9c7ec6cae6a6b8547c29b9255b
Ali Sabanci <alisabanci7@gmail.com>
2023-04-26 15:18:36 +0300

    upload

---
+++ clone_graph.py
@@ -0,0 +1,22 @@
+class Node:
+    def __init__(self, val=0, neighbors=None):
+        self.val = val
+        self.neighbors = neighbors if neighbors is not None else []
+
+class Solution:
+    def cloneGraph(self, node):
+        
+        oldToNew = {}
+
+        def dfs(node):
+            if node not in oldToNew:
+                return oldToNew[node]
+            
+            copy = Node(node.val)
+            oldToNew[node] = copy
+            for nei in node.neighbors:
+                copy.neighbors.append(dfs(nei))
+            return copy
+        
+        return dfs(node) if node else None
+        
