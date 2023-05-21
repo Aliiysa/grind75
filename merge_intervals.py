f4fa15136b29ee1608264c5f682c167092905cd1
Ali Sabanci <alisabanci7@gmail.com>
2023-05-21 18:08:57 +0300

    upload

---
+++ merge_intervals.py
@@ -0,0 +1,12 @@
+def merge(intervals):
+    intervals.sort(key=lambda i: i[0])
+    result = [intervals[0]]
+
+    for start, end in intervals[1:]:
+        lastEnd = result[-1][1]
+        if start <= lastEnd:
+            result[-1][1] = max(lastEnd, end)
+        else:
+            result.append([start, end])
+    
+    return result
