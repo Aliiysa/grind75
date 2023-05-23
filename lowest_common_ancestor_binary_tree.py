0977baf6869d1e1629cf6f1de5e8496796c4a342
Ali Sabanci <alisabanci7@gmail.com>
2023-05-23 11:21:12 +0300

    upload

---
+++ lowest_common_ancestor_binary_tree.py
@@ -0,0 +1,14 @@
+def lowestCommonAncestor(root, p, q):
+    if not root:
+        return None
+    
+    if root == p or root == q:
+        return root
+    
+    left = lowestCommonAncestor(root.left, p, q)
+    right = lowestCommonAncestor(root.right, p, q)
+
+    if left and right:
+        return root
+    else:
+        return left or right
