330ae1698a3a1291d63a8d2d25468aff76943441
Ali Sabanci <alisabanci7@gmail.com>
2023-06-22 11:33:47 +0300

    upload

---
+++ letter_combinations_of_a_phone_number.py
@@ -0,0 +1,18 @@
+def letterCombination(digits):
+    result = []
+
+    digitToChar = {"2": "abc", "3": "def",
+    "4":"ghi", "5":"jkl", "6":"mno",
+    "7":"pqrs", "8":"tuv", "9":"wxyz"}
+
+    def backtrack(i, currStr):
+        if len(currStr) == len(digits):
+            result.append(currStr)
+            return
+        for c in digitToChar[digits[i]]:
+            backtrack(i + 1, currStr + c)
+    
+    if digits:
+        backtrack(0, "")
+    
+    return result
