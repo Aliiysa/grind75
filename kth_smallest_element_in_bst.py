d5809c2b3a69a02b9736bc703493f404c7ec82f4
Ali Sabanci <alisabanci7@gmail.com>
2023-07-10 11:17:52 +0300

    upload

---
+++ kth_smallest_element_in_bst.py
@@ -0,0 +1,15 @@
+def kthSmallest(root, k):
+    n = 0
+    stack = []
+    cur = root
+
+    while cur and stack:
+        while cur:
+            stack.append(cur)
+            cur = cur.left()
+        
+        cur = stack.pop()
+        n += 1
+        if n == k:
+            return cur.val
+        cur = cur.right
